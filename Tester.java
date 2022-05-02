e114dbf819d0c7c5adc58c422045745879e39e12
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2022-05-01 17:34:04 -0700

    add one failed test initially

---
+++ Tester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class tester{
+
+    @Test
+    public static void test1(){
+        assertEquals(3, TestforSkillDemo.add(2,3));
+    }
+}
--- TestforSkillDemo.java
+++ TestforSkillDemo.java
@@ -1,6 +1,9 @@
 public class TestforSkillDemo{
 
-  Lets open the GitHub Desktop!
+  //Lets open the GitHub Desktop!
+  public static int add(int a, int b){
+    return a+b;
+  }
 
 }
 
